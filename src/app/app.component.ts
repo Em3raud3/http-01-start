492e1d45b3e6e7e389d192b3a6be3c5fd6bc03a2
Em3raud3 <animekamisama@gmail.com>
2022-10-05 11:45:56 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  fireBase: string = "https://angular-project-6e2d0-default-rtdb.firebaseio.com/"
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http.post(
+      this.fireBase + 'posts.json',
+      postData
+      ).subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    // Send Http request
+    this.http.get(this.fireBase + 'posts.json').subscribe(posts => {
+      console.log(posts);
+    });
+  }
+
+}
